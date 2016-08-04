b1647062bc0952b582947a3d786703bb4e8dbfed
Laura Silvey <lsilvey@gmail.com>
2016-08-04 08:18:12 -0700

    add links to users and 'closed account' grouping

--- js/script.js
+++ js/script.js
@@ -1,6 +1,6 @@
-var users = ["cretetion", "ESL_SC2", "freecodecamp", "habathcx", "noobs2ninjas", "OgamingSC2", "RobotCaleb"];
+var users = ["brunofin", "cretetion", "ESL_SC2", "freecodecamp", "habathcx", "noobs2ninjas", "OgamingSC2", "RobotCaleb"];
 
-function newBox(id, newClass){
+function newBox(id, name){
   var newP = document.createElement("p");
   var newHeader = document.createElement("h3");
   var newDiv = document.createElement("div");
@@ -10,11 +10,11 @@ function newBox(id, newClass){
   $(id).append(newDiv);
   $(newDiv).addClass("container well");
   $(newDiv).append(rowDiv);
-  $(rowDiv).addClass("row");
+  $(rowDiv).addClass("row " + name + "-row");
   $(rowDiv).append(newImg, newHeader, newP);
-  $(newImg).addClass(newClass + "logo col-sm-3 img-circle img-small");
-  $(newHeader).addClass(newClass + " col-sm-3");
-  $(newP).addClass(newClass + "details col-sm-6 text-right");
+  $(newImg).addClass(name + "logo col-sm-3 img-circle img-small");
+  $(newHeader).addClass(name + " col-sm-4");
+  $(newP).addClass(name + "details col-sm-5 text-center");
 };
 
 
@@ -24,21 +24,29 @@ function allUsers(){
     $.getJSON(url, function(data){
       if(data.stream === Object(data.stream)){
         newBox("#onlineResults", user);
-        $(".well").addClass("onlineResults");
+        $(".well").addClass("online-results");
         $("." + user + "logo").attr("src", data.stream.channel.logo);
         $("." + user).html(user);
+        $("." + user + "-row").wrap('<a href="' + data.stream.channel.url + '" target="blank" />');
         $("." + user + "details").html(data.stream.channel.status);
-      }else{
+      }else if(data.stream === null){
         $.getJSON(data._links.channel, function(data){
           newBox("#offlineResults", user);
           $("." + user + "logo").attr("src", data.logo);
           $("." + user).html(user);
           $("." + user + "details").html("offline");
+          $("." + user + "-row").wrap('<a href="https://www.twitch.tv/' + user + '" target="blank" />')
         });
+      }else{
+        newBox("#accountClosed", user);
+        $("." + user + "logo").attr("src", "https://www.theyearinpictures.co.uk/images//image-placeholder.png");
+        $("." + user).html(user);
+        $("." + user + "details").html("account closed");
       }
     });
   });  
 };
+
 
 
 $(document).ready(function() {
